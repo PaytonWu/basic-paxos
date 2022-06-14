cb5ddf6d070451a09b0508de4b708ae143fcf78d
payton.wu <payton.wu@topnetwork.org>
2022-06-14 21:17:26 +0800

    add Proposer.

--- basicpaxos-cs/BasicPaxosCore/Network.cs
+++ basicpaxos-cs/BasicPaxosCore/Network.cs
@@ -5,7 +5,7 @@ namespace BasicPaxosNet
 {
     class NetworkDriver
     {
-        private const int serviceNumber = 8888;
+        private const Int32 serviceNumber = 8888;
 
         private ICollection<IPAddress> peers;
         private readonly UdpClient self;
@@ -13,13 +13,20 @@ namespace BasicPaxosNet
         public NetworkDriver(IPAddress localAddress)
         {
             self = new UdpClient(new IPEndPoint(localAddress, ServiceNumber));
+            peers = new List<IPAddress>();
         }
 
-        public static int ServiceNumber => serviceNumber;
+        public static Int32 ServiceNumber => serviceNumber;
 
-        void SendTo(Message message, IPAddress peer)
+        public void SendTo(Message message, IPAddress peer)
         {
             self.Send(message.Serialize(), new IPEndPoint(peer, ServiceNumber));
+        }
+
+        public Byte[] Receive()
+        {
+            IPEndPoint? remote = null;
+            return self.Receive(ref remote);
         }
     }
 }
---
+++ basicpaxos-cs/BasicPaxosCore/Proposer.cs
@@ -0,0 +1,35 @@
+﻿using System.Net;
+using BasicPaxosNet;
+class Proposer
+{
+    public Proposer(IPAddress ipAddress, Int32 id, IPAddress[] peerAddresses)
+    {
+        Id = id;
+        PeerAddresses = peerAddresses;
+        NetworkDriver = new NetworkDriver(ipAddress);
+    }
+
+    public Int32 Id { get; private set; }
+    public Int32 Round { get; set; }
+    public Int32 ProposalId { get; set; }
+
+    public IPAddress[] PeerAddresses { get; private set; }
+
+    private NetworkDriver NetworkDriver { get; set; }
+
+    void Propose(Int32 value)
+    {
+        Round++;
+        ProposalId = Round << 16 | Id;
+
+        var messageProposal = new MessageProposal(ProposalId, Id, value);
+        var message = new Message(MessageType.Propose, messageProposal.Serialize());
+        foreach (var peerAddress in PeerAddresses)
+        {
+            NetworkDriver.SendTo(message, peerAddress);
+            var reply = NetworkDriver.Receive();
+
+
+        }
+    }
+}
